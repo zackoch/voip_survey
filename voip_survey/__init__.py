215d585f5f93c5169c59b4884df99b5c8e68c2f4
Zac <zackoch@gmail.com>
2021-02-02 16:03:48 -0600

    move path to init

--- voip_survey/__init__.py
+++ voip_survey/__init__.py
@@ -2,11 +2,10 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import dotenv
-from secrets import token_hex
 
 dotenv.load_dotenv()
 
-path = token_hex(6)
+path = os.getenv('PATH')
 app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
